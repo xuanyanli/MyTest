8bd5868104e3a43fd2b0e1a2cfe8b60e60a66f8f
lixuanyan <2407584419@qq.com>
2017-08-23 20:02:39 -0700

    Initial Commit

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by 李选雁 on 2017/8/23.
+//  Copyright © 2017年 李选雁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestDemo
+//
+//  Created by 李选雁 on 2017/8/23.
+//  Copyright © 2017年 李选雁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
